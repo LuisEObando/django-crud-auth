e356aa9149e793cc9750d4fae0ed76c332df16a6
LuisEObando <luiseobando95@gmail.com>
2024-06-26 09:40:00 -0500

    Se crea ruta para cargar la raíz en home

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
